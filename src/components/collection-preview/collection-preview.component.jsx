58190836fc2a93ee849a86a85998a7403a8dcb47
Pooja Shingala <pooja.shingala007@gmail.com>
2020-05-11 14:12:18 +0530

    updated shop data with new data, updated cart utils to group items in cart reducer

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -10,8 +10,8 @@ const CollectionPreview = ({ title, items }) => (
             {
                 items
                 .filter( (item, idx) => idx < 4 )
-                .map( ({id, ...otherItemPreviewProps}) => 
-                    <CollectionItem key={id} {...otherItemPreviewProps} />)
+                .map( (item) => 
+                    <CollectionItem key={item.id} item={ item } />)
             }
         </div>
     </div>
--- src/redux/cart/cart.actions.js
+++ src/redux/cart/cart.actions.js
@@ -1,7 +1,9 @@
 import CartActionTypes from './cart.types';
 
-const toggleCartHidden = () => ({
+export const toggleCartHidden = () => ({
     type: CartActionTypes.TOGGLE_CART_HIDDEN
-})
-
-export default toggleCartHidden;
+});
+ export const addItem = item => ({
+    type: CartActionTypes.ADD_ITEMS,
+    payload: item
+ }); 
