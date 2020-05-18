7f0e16f46b73c0a59753f145907c92578e1bd174
Pooja Shingala <pooja.shingala007@gmail.com>
2020-05-18 17:50:18 +0530

    updating stripe integration and updating favicon and title of the site

--- src/App.js
+++ src/App.js
@@ -3,16 +3,17 @@ import { Switch, Route, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 
-import './App.css';
+import {auth, createUserProfileDocument} from './firebase/firebase.utils';
+import { setCurrentUser } from './redux/user/user.actions';
+import { selectCurrentUser } from './redux/user/user.selector';
 
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import Header from './components/header/header.component';
-import {auth, createUserProfileDocument} from './firebase/firebase.utils';
-import { setCurrentUser } from './redux/user/user.actions';
-import { selectCurrentUser } from './redux/user/user.selector';
 import CheckoutPage from './components/checkout/checkout.component';
+
+import './App.css';
 
 class App extends React.Component {
 
