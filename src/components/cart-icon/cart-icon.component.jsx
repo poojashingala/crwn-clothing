0ad40091a9bde54ab96fd77856df677ba9c2568e
Pooja Shingala <pooja.shingala007@gmail.com>
2020-05-21 14:20:04 +0530

    Implement css in js functionality using styled components in components

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -5,14 +5,13 @@ import { createStructuredSelector } from 'reselect';
 import { toggleCartHidden } from '../../redux/cart/cart.actions';
 import { selectCartItemCount } from '../../redux/cart/cart.selectors';
 
-import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
-import './cart-icon.styles.scss';
+import { CartIconContainer, ShoppingIconStyles, ItemCountStyles } from './cart-icon.styles';
 
 const CartIcon = ({ toggleCartHidden, itemCount }) => (
-    <div className="cart-icon" onClick={toggleCartHidden}>
-        <ShoppingIcon className="shopping-icon" />
-        <span className="item-count">{itemCount}</span>
-    </div>
+    <CartIconContainer onClick={toggleCartHidden}>
+        <ShoppingIconStyles />
+        <ItemCountStyles>{itemCount}</ItemCountStyles>
+    </CartIconContainer>
 );
 
 const mapStateToProps = createStructuredSelector({
--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,31 +1,33 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import CustomButton from '../custom-button/custom-button.component';
+
 import { addItem } from '../../redux/cart/cart.actions';
 
-import './collection-item.styles.scss';
+import { 
+    CollectionItemContainer, 
+    BackgroundImage, 
+    CollectionFooter, 
+    AddCustomButtonStyles,
+    NameContainer,
+    PriceContainer
+} from "./collection-item.styles";
 
 const CollectionItem = ({ item, addItem }) => {
     const { name, price, imageUrl } = item;
     return(
-        <div className="collection-item">
-            <div 
-            className="image" 
-            style={{
-                backgroundImage: `url(${imageUrl})`
-            }}
-            />
-            <div className="collection-footer">
-                <span className="name">{name}</span>
-                <span className="price">{price}</span>
-            </div>
-            <CustomButton 
+        <CollectionItemContainer>
+        <BackgroundImage className='image' imageUrl={imageUrl} />
+            <CollectionFooter>
+                <NameContainer>{name}</NameContainer>
+                <PriceContainer>{price}</PriceContainer>
+            </CollectionFooter>
+            <AddCustomButtonStyles 
                 onClick={() => addItem(item)} 
                 inverted
             >
                 Add To Cart
-            </CustomButton>
-        </div> 
+            </AddCustomButtonStyles>
+        </CollectionItemContainer> 
     )
 }
 
