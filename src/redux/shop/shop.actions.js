430223be369bbcf981a37811cfe3ce80fac1487f
Pooja Shingala <pooja.shingala007@gmail.com>
2020-05-28 18:22:54 +0530

    Install Redux thunk middleware and move our async code into redux

--- src/redux/shop/shop.actions.js
+++ src/redux/shop/shop.actions.js
@@ -1,6 +1,27 @@
 import ShopActionTypes from './shop.types';
+import { firestore, convertCollectionsSnapshotsToMap } from '../../firebase/firebase.utils';
 
-export const updateCollections = (collectionsMap) => ({
-    type: ShopActionTypes.UPDATE_COLLECTIONS,
+export const fetchCollectionsStart = () => ({
+    type: ShopActionTypes.FETCH_COLLECTIONS_START,
+});
+
+export const fetchCollectionsSuccess = (collectionsMap) => ({
+    type: ShopActionTypes.FETCH_COLLECTIONS_SUCCESS,
     payload: collectionsMap
 });
+
+export const fetchCollectionFailure = errorMessage => ({
+    type: ShopActionTypes.FETCH_COLLECTIONS_FAILURE,
+    payload: errorMessage
+});
+
+export const fetchCollectionsStartAsync = () => {
+    return dispatch => {
+        const collectionRef = firestore.collection('collections');
+        dispatch(fetchCollectionsStart());
+        collectionRef.get().then( snapshot => {
+            const collectionMap = convertCollectionsSnapshotsToMap(snapshot);
+            dispatch(fetchCollectionsSuccess(collectionMap))
+        }).catch(dispatch(error => fetchCollectionFailure(error.message)))
+    }
+}
