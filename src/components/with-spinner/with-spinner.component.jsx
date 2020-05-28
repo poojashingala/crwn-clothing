5b6aa69075622dfaac3c16ea4436b99a3c45320a
Pooja Shingala <pooja.shingala007@gmail.com>
2020-05-28 15:22:43 +0530

    Included with Spinner HOC

---
+++ src/components/with-spinner/with-spinner.component.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+import { SpinnerContainer, SpinnerOverlay } from './with-spinner.styles';
+
+const WithSpinner = WrappedComponent => {
+    const Spinner = ({ isLoading, ...otherProps }) => {
+        return isLoading ? 
+        (<SpinnerOverlay>
+            <SpinnerContainer />
+        </SpinnerOverlay>) :
+        <WrappedComponent { ...otherProps } />
+    }
+    return Spinner;
+}
+
+//Another way to write HOC.
+// const WithSpinner = WrappedComponent => ({ isLoading, ...otherProps }) => {
+//     return isLoading ? 
+//     (<SpinnerOverlay>
+//         <SpinnerContainer />
+//     </SpinnerOverlay>) :
+//     <WrappedComponent { ...otherProps } />
+// }
+
+export default WithSpinner;
--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -7,23 +7,40 @@ import CollectionPage from '../collection/collection.component';
 
 import {firestore, convertCollectionsSnapshotsToMap} from '../../firebase/firebase.utils';
 import { updateCollections } from '../../redux/shop/shop.actions';
+import WithSpinner from '../../components/with-spinner/with-spinner.component';
+
+const CollectionOverviewWithSpinner = WithSpinner(CollectionsOverview);
+const CollectionPageWithSpinner = WithSpinner(CollectionPage);
+
 class ShopPage extends React.Component{
+    state = {
+        loading: true
+    };
+
     unsubscribeFromSnapshot = null;
     componentDidMount(){
         const { updateCollections } = this.props;
         const collectionRef = firestore.collection('collections');
-        this.unsubscribeFromSnapshot = collectionRef.onSnapshot( async snapshot => {
+        
+        collectionRef.get().then( snapshot => {
             const collectionMap = convertCollectionsSnapshotsToMap(snapshot);
             updateCollections(collectionMap);
+            this.setState({ loading: false })
         } )
+        /* this.unsubscribeFromSnapshot = collectionRef.onSnapshot( async snapshot => {
+            const collectionMap = convertCollectionsSnapshotsToMap(snapshot);
+            updateCollections(collectionMap);
+            this.setState({ loading: false })
+        } ) */
     }
 
     render() {
         const { match } = this.props;
+        const { loading } = this.state;
         return(
             <div className="shop-page">
-            <Route exact path={`${match.path}`} component={CollectionsOverview} />
-            <Route path={`${match.path}/:collectionId` } component={CollectionPage} />
+            <Route exact path={`${match.path}`} render={(props) => <CollectionOverviewWithSpinner isLoading={loading} {...props} />} />
+            <Route path={`${match.path}/:collectionId` } render={(props) => <CollectionPageWithSpinner isLoading={loading} {...props} />} />
             </div>
         );
     }
