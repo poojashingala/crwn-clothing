279e716031aa2bddec542dddafc4c75fa91b5057
Pooja Shingala <pooja.shingala007@gmail.com>
2020-06-13 14:43:33 +0530

    separating our app into client folder, added express server to handle stripe payment

--- client/src/components/stripe-button/stripe-button.component.jsx
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -1,20 +1,37 @@
 import React from 'react';
 import StripeCheckout from 'react-stripe-checkout';
+import axios from 'axios';
 
 const StripeCheckoutButton = ({ price }) => {
     const priceForStripe = price * 100;
     const publishableKey = 'pk_test_2OMFumL0hvuPS0nNZdCaucrg00fmu15cOF';
     const onToken = token => {
-        console.log(token);
-        alert('Payment Successful');
-    }
+        axios({
+          url: 'payment',
+          method: 'post',
+          data: {
+            amount: priceForStripe,
+            token: token
+          }
+        })
+          .then(response => {
+            alert('succesful payment');
+          })
+          .catch(error => {
+            console.log('Payment Error: ', error);
+            alert(
+              'There was an issue with your payment! Please make sure you use the provided credit card.'
+            );
+          });
+    };
+
     return(
         <StripeCheckout 
             label="Pay Now"
             name="CRWN CLOTHING LTD"
             billingAddress
             shippingAddress
-            image="https://sendeyo.com/up/d/f3eb2117da"
+            image="https://logo.clearbit.com/stripe.com"
             description={`Your Total amount is $${price}`}
             amount={priceForStripe}
             panelLabel="Pay Now"
